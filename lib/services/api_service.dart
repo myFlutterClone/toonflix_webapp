0744f7281a0dea671395fb0f2641af63eb474743
앙구리 <man3801@gmail.com>
2023-12-07 18:48:52 +0900

    6.3 fromJson End_이해불가

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,17 +1,23 @@
 // Flutter 위젯이나 클래스가 아님
 // 그냥 평범한 Dart 클래스
+import 'dart:convert';
 import 'package:http/http.dart';
+import 'package:toonflix_webapp/models/webtoon_model.dart';
 
 class ApiService {
   final String baseUrl = "https://webtoon-crawler.nomadcoders.workers.dev";
   final String today = "today";
 
-  void getTodaysToons() async {
+  Future<List<WebtoonModel>> getTodaysToons() async {
+    List<WebtoonModel> webtoonInstances = [];
     final url = Uri.parse('$baseUrl/$today');
     final Response = await get(url);
     if (Response.statusCode == 200) {
-      print(Response.body);
-      return;
+      final List<dynamic> webtoons = jsonDecode(Response.body);
+      for (var webtoon in webtoons) {
+        webtoonInstances.add(WebtoonModel.fromJson(webtoon));
+      }
+      return webtoonInstances;
     }
     throw Error();
   }
