b7e28e4b1662b067233194157954fef154f5c2d8
앙구리 <man3801@gmail.com>
2023-12-08 18:36:46 +0900

    6.7 ListView End

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -25,7 +25,7 @@ class HomeScreen extends StatelessWidget {
         future: webtoons,
         builder: (context, snapshot) {
           if (snapshot.hasData) {
-            return ListView.builder(
+            return ListView.separated(
               scrollDirection: Axis.horizontal,
               itemCount: snapshot.data!.length,
               itemBuilder: (context, index) {
@@ -33,6 +33,7 @@ class HomeScreen extends StatelessWidget {
                 print(index);
                 return Text(webtoon.title);
               },
+              separatorBuilder: (context, index) => const SizedBox(width: 20),
             );
           }
           return const Center(
