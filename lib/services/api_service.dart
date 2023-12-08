a717a7b19bff5013901a6a0037323a14811e7bf8
앙구리 <man3801@gmail.com>
2023-12-08 15:54:59 +0900

    6.5 waitForWebToons End

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -5,15 +5,16 @@ import 'package:http/http.dart';
 import 'package:toonflix_webapp/models/webtoon_model.dart';
 
 class ApiService {
-  final String baseUrl = "https://webtoon-crawler.nomadcoders.workers.dev";
-  final String today = "today";
+  static const String baseUrl =
+      "https://webtoon-crawler.nomadcoders.workers.dev";
+  static const String today = "today";
 
-  Future<List<WebtoonModel>> getTodaysToons() async {
+  static Future<List<WebtoonModel>> getTodaysToons() async {
     List<WebtoonModel> webtoonInstances = [];
     final url = Uri.parse('$baseUrl/$today');
-    final Response = await get(url);
-    if (Response.statusCode == 200) {
-      final List<dynamic> webtoons = jsonDecode(Response.body);
+    final response = await get(url);
+    if (response.statusCode == 200) {
+      final List<dynamic> webtoons = jsonDecode(response.body);
       for (var webtoon in webtoons) {
         webtoonInstances.add(WebtoonModel.fromJson(webtoon));
       }
