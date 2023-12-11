bf1d2bc1a638773f6fc8b80038dfe4365f2c4325
앙구리 <man3801@gmail.com>
2023-12-11 17:35:48 +0900

    6.12 ApiService End

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -3,6 +3,8 @@
 import 'dart:convert';
 import 'package:http/http.dart';
 import 'package:toonflix_webapp/models/webtoon_model.dart';
+import 'package:toonflix_webapp/models/webtoon_detail_model.dart';
+import 'package:toonflix_webapp/widgets/webtoon_widget.dart';
 
 class ApiService {
   static const String baseUrl =
@@ -19,6 +21,31 @@ class ApiService {
         webtoonInstances.add(WebtoonModel.fromJson(webtoon));
       }
       return webtoonInstances;
+    }
+    throw Error();
+  }
+
+  static Future<WebtoonDetailModel> getToonById(String id) async {
+    final url = Uri.parse("$baseUrl/$id");
+    final response = await get(url);
+    if (response.statusCode == 200) {
+      final webtoon = jsonDecode(response.body);
+      return WebtoonDetailModel.fromJson(webtoon);
+    }
+    throw Error();
+  }
+
+  static Future<List<WebtoonDetailModel>> getLatestEpisodesById(
+      String id) async {
+    List<WebtoonDetailModel> episodesInstances = [];
+    final url = Uri.parse("$baseUrl/$id/episodes");
+    final response = await get(url);
+    if (response.statusCode == 200) {
+      final episodes = jsonDecode(response.body);
+      for (var episode in episodes) {
+        episodesInstances.add(WebtoonDetailModel.fromJson(episode));
+      }
+      return episodesInstances;
     }
     throw Error();
   }
